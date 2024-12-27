b552e4c3ad99490e48f2fcafe1548b8f44035f43
Jeel Patel <jeelp9216@gmail.com>
2024-12-28 00:17:05 +0530

    [UPDATED] Fixed password hash and added authentication tokens

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -2,11 +2,11 @@ import ApiResponse from '../utils/apiResponse.js'
 import ApiError from '../utils/apiError.js'
 import AsyncHandler from '../utils/asyncHandler.js'
 import { User } from '../models/users.model.js';
+import jwt from 'jsonwebtoken';
 
 const registerController = AsyncHandler(async(req, res)=>{
     const {email, password} = req.body;
 
-    console.log("step0");
     if([email, password].some((field)=>{field.trim() === ""})){
         throw new ApiError(400, "All field are required.")
     }
@@ -24,8 +24,94 @@ const registerController = AsyncHandler(async(req, res)=>{
 
 });
 
+const generateAccessAndRefreashToken = async (userId)=>{
+    try{
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken
+        await user.save();
+        return {accessToken, refreshToken};
+    }
+    catch(err){
+        throw new ApiError(500, "Something went wrong while generating access and refresh token.");
+    }
+}
+
 const loginController = AsyncHandler(async(req, res)=>{
-    
+
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        throw new ApiError(400, "email and password is essential.");
+    }
+
+
+    const user = await User.findOne({email});
+
+    if(!user) throw new ApiError(404, 'User not found.');
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+    if(!isPasswordValid) throw new ApiError(401, 'Invalid credential.')
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreashToken(user._id);
+
+    const loggedUser = await User.findById(user._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly:  true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie('accesstToken', accessToken, options)
+    .cookie('refreshToken', refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedUser, accessToken, refreshToken
+            },
+            'User logged in successfully.'
+        )
+    )
 });
 
-export {registerController, loginController};
+const refreshAcessToken = AsyncHandler(async(req, res)=>{
+    const incomingRefreshToken = req.cookie.refreshToken || req.body.refreshToken;
+
+    if(!incomingRefreshToken) throw new ApiError(401, 'unauthorized request.');
+
+    try{
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env, REFRESH_TOKEN_SECRET); 
+        const user = await User.findById(decodedToken?._id);
+
+        if(!user || incomingRefreshToken !== user?.refreshToken) throw new ApiError(401, 'Invalid refresh token.');
+
+        const options = {
+            http: true,
+            secure: true
+        }
+
+        const {accessToken, newRefreshToken} = await generateAccessAndRefreashToken(user._id);
+
+        return res
+        .status(200)
+        .cookie('accessToken', accessToken, options)
+        .cookie('refreshToken', refreshToken, options)
+        .json(
+            new ApiResponse(200,
+                { accessToken, refreshToken: newRefreshToken},
+                'access token is refreshed.'
+            )
+        )
+    }
+    catch(err){
+        throw new ApiError(401, 'invalid refresh token.');
+    }
+
+})
+
+export {registerController, loginController, refreshAcessToken};
--- server/src/models/users.model.js
+++ server/src/models/users.model.js
@@ -20,14 +20,14 @@ const userSchema = new Schema(
 )
 
 userSchema.pre("save", async function (next){
-    if(this.isModified('password')) return next();
+    if(!this.isModified('password')) return next();
 
-    this.password = bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 })
 
 userSchema.methods.isPasswordCorrect = async function (password){
-    return await bcrypt.compare(this.password, password);
+    return await bcrypt.compare(password, this.password);
 }
 
 userSchema.methods.generateAccessToken = function (){
